cba4c49ef4ab2d686140d3e5eb7b8fcd5126994a
VivekRatan_Shakya <113957044+VivekRatan-Shakya@users.noreply.github.com>
2024-02-18 15:55:27 +0530

    Add files via upload

---
+++ Completed WebSites/Main_Folder/js/main.js
@@ -0,0 +1,22 @@
+// header scroll  Topbar Border
+
+
+let nav = document.querySelector(".navbar");
+ window.onscroll = function() {
+    if(document.documentElement.scrollTop >20){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+ }
+
+// nav hide  click after hide navmenu
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+       navCollapse.classList.remove("show");
+    })
+})
+  
